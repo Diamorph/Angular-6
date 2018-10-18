c7f4c0b38828e5d0237c192a6faacdcc1e4f5675
Vlad Tymoshenko <diamorph1309@gmail.com>
2018-10-18 15:01:01 +0300

    move to SASS

--- Notepad/src/app/notepad/notepad.component.ts
+++ Notepad/src/app/notepad/notepad.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notepad',
   templateUrl: './notepad.component.html',
-  styleUrls: ['./notepad.component.css']
+  styleUrls: ['./notepad.component.scss']
 })
 export class NotepadComponent {
   test = 'Notepad';
--- Notepad/src/app/scheduler/scheduler.component.ts
+++ Notepad/src/app/scheduler/scheduler.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-scheduler',
   templateUrl: './scheduler.component.html',
-  styleUrls: ['./scheduler.component.css']
+  styleUrls: ['./scheduler.component.scss']
 })
 export class SchedulerComponent {
   test = 'scheduler';
